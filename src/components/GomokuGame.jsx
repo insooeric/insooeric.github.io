fe45e6d632901be20e349e40ebe2b42f5b19159d
Geek <insooeric.son@gmail.com>
2025-01-25 19:07:03 -0500

    minor change on gomoku

--- src/components/GomokuGame.jsx
+++ src/components/GomokuGame.jsx
@@ -397,7 +397,7 @@ const GomokuGame = () => {
             className="last-move-marker"
             style={{
               top: lastMove.Row * cellSize + (cellSize / 2 - 6),
-              left: lastMove.Col * cellSize + (cellSize / 2 - 6),
+              left: lastMove.Col * cellSize + (cellSize / 2 - 5),
             }}
           ></div>
         )}
